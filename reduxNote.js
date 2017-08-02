517b746d247f6236ca95cc10daee1e479494dbf2
Trina Haque <v-trhaqu@microsoft.com>
2017-08-02 12:33:59 -0700

    store and dispatch

--- reduxNote.js
+++ reduxNote.js
@@ -14,4 +14,27 @@ const reducerFunction = (state = 0, action) => {
 
 // Needs three things at the store
 const { createStore } = Redux;
+
+// store takes in a reducer function
 const store = createStore(reducerFunction);
+
+
+// first method is getState that helps us get the initial state
+store.getState();
+
+// dispatch allows to change action
+store.dispatch({type: 'Increment'});
+
+
+// needs this to render the initial state
+const render = () => {
+    document.body.innerText = store.getState();
+}
+
+store.subscribe(render);
+render();
+
+
+document.addEventListener('click', () => {
+    store.dispatch({type: "Increment" });
+})
